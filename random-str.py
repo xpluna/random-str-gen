79c36f554513675166f1684921cdc5a4884973aa
karldino <76738691+karldino@users.noreply.github.com>
2021-07-03 21:14:59 +0530

    Beautified code with online formatter

--- random-str.py
+++ random-str.py
@@ -1,11 +1,16 @@
+
 import random
 
 chars = list(
-    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()`~-_=+\,.<>/?;:\'"')
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()`~-_=+\,.<>/?;:'\""
+)
 strs = []
 
-classif = int(input(
-    '[1] Generate string with manual lengths?\n[2] Make the program generate strings with number\n    of outputs you provide?\n\n>>> '))
+classif = int(
+    input(
+        "[1] Generate string with manual lengths?\n[2] Make the program generate strings with number\n    of outputs you provide?\n\n>>> "
+    )
+)
 
 if classif == 1:
 
@@ -14,12 +19,12 @@ if classif == 1:
     while True:
         n = input("\n>>> ")
 
-        if n.lower().strip() == 'q':
+        if n.lower().strip() == "q":
             break
 
         else:
             i = 0
-            message = ''
+            message = ""
             while i != int(n):
                 message += random.choice(chars)
                 i += 1
@@ -27,25 +32,26 @@ if classif == 1:
             print(message)
             strs.append(message)
 
-    with open('strings.txt', 'w') as fobj:
+    with open("strings.txt", "w") as fobj:
         for string in strs:
-            fobj.write(f'{string}\n\n')
+            fobj.write(f"{string}\n\n")
 
 elif classif == 2:
-    
-    Os = int(input('\nNumber of strings to export\n>>> '))
-    Ls = int(input('\nLength of each string\n>>> '))
+
+    Os = int(input("\nNumber of strings to export\n>>> "))
+    Ls = int(input("\nLength of each string\n>>> "))
     i = 0
-    
+
     while Os != 0:
         i = 0
-        message = ''
+        message = ""
         while i != Ls:
             message += random.choice(chars)
             i += 1
         strs.append(message)
         Os -= 1
-    
-    with open('strings.txt', 'w') as fobj:
+
+    with open("strings.txt", "w") as fobj:
         for string in strs:
-            fobj.write(f'{string}\n\n')
+            fobj.write(f"{string}\n\n")
+
